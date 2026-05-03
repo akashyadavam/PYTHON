9a9f4ba489f2f500c8e2c0489afe45d02d3817ff
akashyadavam <akashyadavam585ak@gmail.com>
2026-05-03 22:24:58 +0530

    updated

--- part5.py
+++ part5.py
@@ -1,29 +1,22 @@
-# file heandling error 
-
 # a=int(input("tell your number:-"))
 # try:
-#     print(10/a)
-# # except ZeroDivisionError:
-# #     print("sorry")
-# except Exception as er:
+#     print(10//a)
+# # # except ZeroDivisionError:
+# # #     print("sorry")
+# except Exception as er: 
+#     # ya to upar wala used kr lo jab error pta ho warna niche wala used kr lo isase age ka line wala code chalega
 #     print(f"error is {er}")
 # print("hello boss")
 
-
-# a=int(input("tell your number:-"))
+# b=input("no eneter kr de")
 # try:
-#     print(10/a)
-# # except ZeroDivisionError:
-# #     print("sorry")
+#     print(10//b)
 # except Exception as er:
-#     print(f"error is {er}")
+#     print(f"there will be some {er}")
 # else:
-#     print("there is no error")
+#     print("no error")
 # finally:
-#     print("mai always run")
-# print("hello boss")
-
-
+#     print("mai to hamesha run ")
 
 
 # age=int(input("tell youur age:-"))
@@ -42,14 +35,16 @@
 
 # age=int(input("tell youur age:-"))
 # try:
-#     if age<10 or age>18:
+#     print(10/age)
+#     if age>=0 or age<=18:
 #         raise ValueError("nahi milega admission 10 se chota 18 se bado ko")
 #     else:
 #         print("welcome to madrid")
 # except Exception as err:
 #         print(f" error occured as {err} ")
         
-
+# pehle ye chalega
+# if condition:   # ye tabhi check hoga jab upar wali line successful ho ek time pr ek hi error ayega do error sath sath kabhi nahi ayega 
 
 # print("hello")
 
@@ -58,11 +53,21 @@
 
 # p=open('part4.py')
 # print(p.read())
+# p.close()
 
+
+
+# p=open('main.py','r')
+# sirf open krta hai kitab open hai dekha sab  but read nahi
+# print(p)
+# print p object honga andar ka print ke liye apko p.read() ab read krega print
 
 # r=open("superman.txt",'w')
 # r.write("hello")
 
+# r=open("superman.txt",'a')
 # r.write("hello add karo")
 
 
+# r=open("superman.txt",'x')
+# r.write("hello add k   khbjsghakharo")
